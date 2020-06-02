36cd2a2723742f0a248c7e6dac0b830b372b521c
Syalcindag <40965702+Syalcindag@users.noreply.github.com>
2020-06-02 12:37:00 +0300

    NavParams kullanımı ile gelen datayı alma

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, NavParams } from 'ionic-angular';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  sayfa?: any;
+  constructor(public navCtrl: NavController,
+              private navParams: NavParams) {
 
-  constructor(public navCtrl: NavController) {
 
+    this.sayfa = navParams.get('data');
+    console.log(this.sayfa);
   }
 
 }
